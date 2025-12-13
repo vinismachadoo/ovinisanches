b2a12d6fde5fa7291a1c42933623cd062ac5b4bc
Vinicius Sanches <vinicius.sanches@poli.ufrj.br>
2025-12-13 14:08:44 -0300

    fix: suspense boundary

--- app/projects/brasileirao-2025/page.tsx
+++ app/projects/brasileirao-2025/page.tsx
@@ -1,11 +1,14 @@
 import { EvolutionChart } from './components/evolution-chart';
+import * as React from 'react';
 
 export default function Brasileirao2025Page() {
   return (
     <div className="container mx-auto p-10 h-svh flex flex-col gap-y-4 items-center">
       <h1 className="text-3xl font-bold mb-2">Brasileirão 2025</h1>
 
-      <EvolutionChart />
+      <React.Suspense>
+        <EvolutionChart />
+      </React.Suspense>
     </div>
   );
 }
